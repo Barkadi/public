c8ac81393b8e2a5d0385e57c7f0222344e653afb
k.saecherl <k.saecherl@141.19.144.27>
2015-04-28 15:00:08 +0200

    Hello World

---
+++ EclipseEinfuehrung/helloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args) {	
+		//Das ist ein Kommentar
+		int i=0;
+		System.out.println("syso als Abkürzung");
+		if (1 == 1) {
+			i++;
+			System.out.println("asd");
+			System.out.println("asdasd");
+		}
+	}
+}
